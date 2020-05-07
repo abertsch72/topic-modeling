ac065750b445adec735e3e29726316a946acdbdb
Amanda Bertsch <abertsch72@gmail.com>
2020-05-06 22:13:51 -0700

    (slow) first CUR decomposition added

--- entrypoint.py
+++ entrypoint.py
@@ -8,9 +8,31 @@ from nltk.corpus import words
 import itertools
 import cur
 import time
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import NMF
 
-def wait():
-    input("break: ")
+NUM_TOPICS = 6
+
+
+def col_select(num, arr):
+    cols_chosen = []
+    cols_chosen.append(np.random.randint(0, arr.shape[1]))
+    c = arr[:, cols_chosen[0]]
+    while(len(cols_chosen) < num):
+        next = np.random.randint(0, arr.shape[1])
+        if(next not in cols_chosen):
+            cols_chosen.append(next)
+            col = arr[:, next]
+            c = np.column_stack([c, col])
+    return c
+
+def CUR_fit_transform(data, k):
+    c = col_select(k, data)
+    r = np.transpose(col_select(k, np.transpose(data)))
+    u = r @ np.linalg.pinv(data) @ c
+    a = c @ np.linalg.pinv(u) @ r
+    return c, u, r
 
 def sum_cat(data):
     num_each = [0, 0, 0, 0, 0, 0]
@@ -52,44 +74,47 @@ def find_num_in_each_cat(data):
 
     return num_each, correct
 
+
 def find_top_n(result, n):
     top = []
 
     for i in range(len(result)):
         curr_max = []
         for j in range(len(result[i])):
-            if(len(curr_max) < n):
+            if len(curr_max) < n:
                 curr_max.append([j, result[i][j]])
             else:
                 for k in range(len(curr_max)):
-                    if(result[i][j] > curr_max[k][1]):
+                    if result[i][j] > curr_max[k][1]:
                         curr_max[k] = [j, result[i][j]]
                         break
         top.append([c[0] for c in curr_max])
     return top
 
+
 def permute_labels(results, perm=1):
     for i in range(len(results)):
-        if(type(results[i]) == int):
+        if (type(results[i]) == int):
             results[i] = (results[i] + perm) % 6
         else:
             for j in range(len(results[i])):
                 results[i][j] = (results[i][j] + perm) % 6
     return results
 
+
 def validate_firm(results, correct, num_each):
     results = find_top_n(results, 1)
     results = [r[0] for r in results]
     accuracy = []
-    precision = [[],[],[],[],[],[]]
-    recall = [[],[],[],[],[],[]]
+    precision = [[], [], [], [], [], []]
+    recall = [[], [], [], [], [], []]
     permutations = itertools.permutations([0, 1, 2, 3, 4, 5])
 
     for per in permutations:
         true_pos = [0, 0, 0, 0, 0, 0]
         guessed = [0, 0, 0, 0, 0, 0]
         for j in range(len(results)):
-            if(correct[j] == per[results[j]]):
+            if correct[j] == per[results[j]]:
                 true_pos[correct[j]] += 1
             guessed[results[j]] += 1
         accuracy.append(sum(true_pos) / len(results))
@@ -98,16 +123,17 @@ def validate_firm(results, correct, num_each):
             recall.append([])
             i = len(precision) - 1
             if guessed[k] != 0:
-                precision[i].append(true_pos[k]/guessed[k])
+                precision[i].append(true_pos[k] / guessed[k])
             else:
                 precision[i].append(0)
-            recall[i].append(true_pos[k]/num_each[k])
+            recall[i].append(true_pos[k] / num_each[k])
 
     print("STRICT VALIDATION: RESULTS")
     print(max(accuracy))
     m = accuracy.index(max(accuracy))
     print(precision[m])
     print(recall[m])
+
 
 def validate_lax(results, correct, num_each):
     results = find_top_n(results, 2)
@@ -144,6 +170,51 @@ def validate_lax(results, correct, num_each):
     print(precision[m])
     print(recall[m])
 
+def run_validate_SVD(vectorizer, data, correct_labels, num_each):
+    time_start = time.time()
+    svd_model = TruncatedSVD(n_components=6, algorithm='randomized', n_iter=100, random_state=122)
+    results = svd_model.fit_transform(data)
+    time_end = time.time()
+    validate_lax(results, correct_labels, num_each)
+    validate_firm(results, correct_labels, num_each)
+
+    print("*" * 70)
+    print("TIME TAKEN: " + str(time_end - time_start) + " milliseconds")
+    print("*" * 70)
+    print("TOP TERMS:")
+    terms = vectorizer.get_feature_names()
+    for i, comp in enumerate(svd_model.components_):
+        terms_comp = zip(terms, comp)
+        sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:7]
+        print("Topic " + str(i) + ": ")
+        for t in sorted_terms:
+            print(t[0], end=' ')
+        print("")
+    print("\n\n\n")
+
+
+def run_validate_NMF(vectorizer, data, correct_labels, num_each):
+    time_start = time.time()
+    nmf_model = NMF(n_components=6, max_iter=100, random_state=122)
+    nmf_results = nmf_model.fit_transform(data)
+    time_end = time.time()
+
+    print("NMF RESULTS: \n" + "-" * 70)
+    validate_lax(nmf_results, correct_labels, num_each)
+    validate_firm(nmf_results, correct_labels, num_each)
+    print("*" * 70)
+    print("TIME TAKEN: " + str(time_end - time_start) + " milliseconds")
+    print("*" * 70)
+    print("TOP TERMS:")
+    terms = vectorizer.get_feature_names()
+    for i, comp in enumerate(nmf_model.components_):
+        terms_comp = zip(terms, comp)
+        sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:7]
+        print("Topic " + str(i) + ": ")
+        for t in sorted_terms:
+            print(t[0], end=' ')
+        print("")
+    print("\n\n\n")
 
 def start():
     newsgroups = fetch_20newsgroups(shuffle=True, remove=('headers', 'footers', 'quotes'), random_state=1)
@@ -154,38 +225,21 @@ def start():
 
     vectorizer = TfidfVectorizer(stop_words='english', max_df=0.5, smooth_idf=True, max_features=1000)
     X = vectorizer.fit_transform(clean)
-
-    terms = vectorizer.get_feature_names()
-
-    from sklearn.decomposition import TruncatedSVD
-
-    # SVD represent documents and terms in vectors
-    svd_model = TruncatedSVD(n_components=6, algorithm='randomized', n_iter=100, random_state=122)
-    results = svd_model.fit_transform(X)
-    validate_lax(results, correct_labels, num_each)
-    validate_firm(results, correct_labels, num_each)
-
-    len(svd_model.components_)
-
-    for i, comp in enumerate(svd_model.components_):
-        terms_comp = zip(terms, comp)
-        sorted_terms = sorted(terms_comp, key=lambda x: x[1], reverse=True)[:7]
-        print("Topic " + str(i) + ": ")
-        for t in sorted_terms:
-            print(t[0], end=' ')
-        print("")
-
-    from sklearn.decomposition import NMF
-    nmf_model = NMF(n_components=6, max_iter=100, random_state=122)
-    nmf_results = nmf_model.fit_transform(X)
-
-    validate_lax(nmf_results, correct_labels, num_each)
-    validate_firm(nmf_results, correct_labels, num_each)
+    #run_validate_SVD(vectorizer, X, correct_labels, num_each)
+    #run_validate_NMF(vectorizer, X, correct_labels, num_each)
+    c, u, r = CUR_fit_transform(X.toarray(), NUM_TOPICS)
+    print(c.shape)
+    print(u.shape)
+    print(r.shape)
+    print(X.toarray().shape)
     #C, U, R = cur.cur_decomposition(X, 6)
-
-    import umap
-    import matplotlib.pyplot as plt
+    print(cur_result := (X.toarray() @ np.linalg.pinv(r) @ u))
+    validate_firm(cur_result, correct_labels, num_each)
+    validate_lax(cur_result, correct_labels, num_each)
     """
+    import umap 
+    import matplotlib.pyplot as plt
+    
     X_topics = svd_model.fit_transform(X)
     embedding = umap.UMAP(n_neighbors=150, min_dist=0.5, random_state=12).fit_transform(X_topics)
 
@@ -208,9 +262,10 @@ def clean_data(raw):
     for item in raw:
         item = re.sub("[^a-zA-Z#]", " ", item)
         item = item.lower()
-        clean_data.append(' '.join([w for w in item.split() if (len(w) > 3 and w not in stop_words and w in dictionary)]))
+        clean_data.append(
+            ' '.join([w for w in item.split() if (len(w) > 3 and w not in stop_words and w in dictionary)]))
 
     return clean_data
 
-start()
 
+start()
